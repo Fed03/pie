cfb22ffe9bd38d006f97e48b63284099c32010b1
Federico Teotini <templarblack.fed03@gmail.com>
2016-03-19 18:37:30 +0100

    Mui css framework

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import(app.bowerDirectory + '/mui/packages/cdn/css/mui.css');
+  app.import(app.bowerDirectory + '/mui/packages/cdn/js/mui.js');
+
   return app.toTree();
 };
