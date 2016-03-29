8650fcfd864d8a15e64e4c11dc5f0b4424550c68
Federico Teotini <templarblack.fed03@gmail.com>
2016-03-29 17:37:29 +0200

    isTesting flag for wormhole

--- app/components/category-selector-input.js
+++ app/components/category-selector-input.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
   tagName: 'span',
   classNames: ['category-selector-input'],
   drawerOpened: false,
+  isTesting: config.environment === 'test',
   click() {
     this.toggleProperty('drawerOpened');
   }
