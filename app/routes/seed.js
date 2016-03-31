b27e4ff33a27a2f135594081e08cf075e5b8fffd
Federico Teotini <templarblack.fed03@gmail.com>
2016-03-31 12:24:51 +0200

    Fixed seeds

--- app/routes/seed.js
+++ app/routes/seed.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 import faker from 'faker';
 
 export default Ember.Route.extend({
-  seed() {
+  beforeModel() {
+    let promises = [];
     for (var i = 0; i < 4; i++) {
       let cat = this.store.createRecord('category', {
         name: faker.commerce.department(),
         type: faker.random.arrayElement(['income', 'outcome'])
       });
-      
-      cat.save();
+
+      promises.push(cat.save());
     }
+
+    return Ember.Promise.all(promises);
   }
 });
