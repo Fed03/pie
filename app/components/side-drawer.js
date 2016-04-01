3d995dfb5a41ebece1d00f7ed0f62e817efb4f1b
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-01 21:45:14 +0200

    Refactored side drawer handling

--- app/components/side-drawer.js
+++ app/components/side-drawer.js
@@ -5,10 +5,14 @@ const { computed } = Ember;
 export default Ember.Component.extend({
   tagName: 'aside',
   classNames: ['sidedrawer'],
-  classNameBindings: ['positionClass'],
+  classNameBindings: ['positionClass', 'visibility'],
   position: 'left',
   isOpen: true,
-  bodyEl: document.getElementsByTagName('body')[0],
+  visibility: computed('isOpen', {
+    get() {
+      return this.get('isOpen') ? 'sidedrawer-opened' : 'sidedrawer-closed';
+    }
+  }),
 
   positionClass: computed('position', {
     get() {
@@ -16,37 +20,51 @@ export default Ember.Component.extend({
     }
   }),
 
-  didRender() {
-    this.$().siblings().addClass('sidedrawer-sibling');
-    this._setTopProperty();
-    this._setSiblingClasses();
-  },
-
-  didReceiveAttrs() {
-    this._super(...arguments);
-    this._toggleVisibility();
-  },
-
-  _toggleVisibility() {
-    const body = this.get('bodyEl');
-    if (this.get('isOpen')) {
-      body.classList.remove('sidedrawer-closed');
-      body.classList.add('sidedrawer-opened');
-    } else {
-      body.classList.remove('sidedrawer-opened');
-      body.classList.add('sidedrawer-closed');
+  $parent: computed('parentSelector', {
+    get() {
+      const parent = Ember.$(this.get('parentSelector'));
+      return parent.length ? parent : Ember.$(this.element).parent();
     }
+  }),
+
+  $sibling: computed('$parent', {
+    get() {
+      return this.get('$parent').children('.sidedrawer-sibling');
+    }
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this._setTopCssProperty();
+    this._setSiblingPositionClass();
+    this._toggleSiblingVisibility();
   },
 
-  _setTopProperty() {
-    const parentPosition = this.$().parent().position();
+  didUpdate() {
+    this._super(...arguments);
+    this._setSiblingPositionClass();
+    this._toggleSiblingVisibility();
+  },
+
+  _setTopCssProperty() {
+    const parentPosition = this.get('$parent').position();
     this.$().css({
       top: parentPosition.top
     });
   },
 
-  _setSiblingClasses() {
-    this.$().siblings().addClass('sidedrawer-sibling')
+  _setSiblingPositionClass() {
+    this.get('$sibling').addClass('sidedrawer-sibling')
       .addClass(`sidedrawer-sibling-${this.get('position')}`);
+  },
+
+  _toggleSiblingVisibility() {
+    let sibling = this.get('$sibling');
+    if (this.get('isOpen')) {
+      sibling.addClass('sidedrawer-sibling-opened');
+    } else {
+      sibling.removeClass('sidedrawer-sibling-opened');
+    }
   }
 });
