a1c94afa4e704b6804e38636eb39193e8c21d7e7
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-06 22:44:44 +0200

    Fixed wrong import in createList test helper

--- tests/helpers/create-list.js
+++ tests/helpers/create-list.js
@@ -3,6 +3,6 @@ import { makeList } from 'ember-data-factory-guy';
 
 export default Ember.Test.registerAsyncHelper('createList', function(app, modelName, num, options) {
   return Ember.run(() => {
-    return Ember.Promise.all(makeList(modelName, num, options).map(model => model.save()));
+    return Ember.RSVP.Promise.all(makeList(modelName, num, options).map(model => model.save()));
   });
 });
