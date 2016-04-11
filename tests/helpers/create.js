e394242bf41754dee63909ab1c2f33a4725ac7e7
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-11 12:29:35 +0200

    Updated create and createList test helpers to handle traits

--- tests/helpers/create.js
+++ tests/helpers/create.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import { make } from 'ember-data-factory-guy';
 
-export default Ember.Test.registerAsyncHelper('create', function(app, modelName, options) {
-  return Ember.run(() => make(modelName, options).save());
+export default Ember.Test.registerAsyncHelper('create', function(app, ...modelArgs) {
+  return Ember.run(() => make(...modelArgs).save());
 });
