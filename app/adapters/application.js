f4fc8893dd698f03579f96cb1c14d0de491cb7f5
Federico Teotini <templarblack.fed03@gmail.com>
2016-04-27 23:12:15 +0200

    Promise based createDB to wait sync

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,32 +1,59 @@
-import { Adapter } from 'ember-pouch';
+import {
+  Adapter
+} from 'ember-pouch';
 import PouchDB from 'pouchdb';
 import config from 'offline-app/config/environment';
 import Ember from 'ember';
 
-const { assert, isEmpty } = Ember;
+const {
+  assert,
+  isEmpty,
+  RSVP: {
+    Promise
+  }
+} = Ember;
 
-function createDb() {
+function createDb(adapter) {
   let localDb = config.emberPouch.localDb;
-
   assert('emberPouch.localDb must be set', !isEmpty(localDb));
 
   let db = new PouchDB(localDb);
+  adapter.set('db', db);
 
   if (config.emberPouch.remoteDb) {
     let remoteDb = new PouchDB(config.emberPouch.remoteDb);
+    return db.allDocs().then(result => {
+      if (result.total_rows !== 0) {
+        db.sync(remoteDb, {
+          live: true,
+          retry: true
+        });
 
-    db.sync(remoteDb, {
-      live: true,
-      retry: true
+        return Promise.resolve();
+      }
+
+      let counter = 0;
+      return new Promise(function(resolve) {
+        db.sync(remoteDb, {
+          live: true,
+          retry: true
+        }).on('paused', function() {
+          if (counter === 1) {
+            resolve();
+          } else {
+            counter++;
+          }
+        });
+      });
     });
   }
 
-  return db;
+  return Promise.resolve();
 }
 
 export default Adapter.extend({
   init() {
     this._super(...arguments);
-    this.set('db', createDb());
+    this.set('promise', createDb(this));
   }
 });
