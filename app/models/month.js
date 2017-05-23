054b796bb9f7aedef18cd6697fb1f3dfdddaa965
Federico Teotini <templarblack.fed03@gmail.com>
2017-05-23 23:03:09 +0200

    month test

---
+++ app/models/month.js
@@ -0,0 +1,35 @@
+import Model from 'ember-pouch/model';
+import DS from 'ember-data';
+import Ember from 'ember';
+import getDateForCurrentMonth from '../utils/get-date-for-current-month';
+
+const {
+  attr,
+  hasMany,
+  // belongsTo
+} = DS;
+
+const months = [
+  'january', 'february', 'march', 'april', 'may', 'june',
+  'july', 'august', 'september', 'october', 'november', 'december'
+];
+
+export default Model.extend({
+  date: attr('date', { defaultValue: getDateForCurrentMonth }),
+  transactions: hasMany('transaction'),
+  openingBalance: attr('number'),
+
+  name: Ember.computed('date', {
+    get() {
+      return months[this.get('date').getUTCMonth()];
+    }
+  }).readOnly(),
+
+  balance: Ember.computed('transactions.@each.value', {
+    get() {
+      return this.get('transactions').mapBy('value').reduce((accumulator, value) => {
+        return accumulator + value;
+      }, 0);
+    }
+  })
+});
---
+++ tests/factories/transaction.js
@@ -0,0 +1,20 @@
+import FactoryGuy from 'ember-data-factory-guy';
+import faker from 'faker';
+
+FactoryGuy.define('transaction', {
+  default: {
+    description: FactoryGuy.generate(() => faker.commerce.product()),
+    value: FactoryGuy.generate(() => faker.commerce.price()),
+    category: FactoryGuy.belongsTo('category')
+  },
+  traits: {
+    yesterday: {
+      date: function() {
+        const yesterday = new Date();
+        yesterday.setUTCHours(0,0,0,0);
+        yesterday.setUTCDate(yesterday.getUTCDate() - 1);
+        return yesterday;
+      }()
+    }
+  }
+});
