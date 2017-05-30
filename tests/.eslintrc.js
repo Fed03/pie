757449366c80d1fce7ad40f54b9f3b4d872a1f33
Federico Teotini <templarblack.fed03@gmail.com>
2017-05-30 22:24:02 +0200

    Eslint custom test helpers

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   env: {
     embertest: true
+  },
+  globals: {
+    createList: false,
+    create: false,
+    findLatestInDb: false,
+    fillTransactionValue: false
   }
 };
