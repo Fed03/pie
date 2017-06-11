159d6fb44e74c909631005f609b0813b33b935c6
Federico Teotini <templarblack.fed03@gmail.com>
2017-06-11 16:30:16 +0200

    eslint and native helpers in category-badge test

--- tests/integration/components/category-badge-test.js
+++ tests/integration/components/category-badge-test.js
@@ -1,51 +1,91 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
-import Ember from 'ember';
+import { findAll, find } from "ember-native-dom-helpers";
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import Ember from "ember";
 
-moduleForComponent('category-badge', 'Integration | Component | category badge', {
-  integration: true
-});
+moduleForComponent(
+  "category-badge",
+  "Integration | Component | category badge",
+  {
+    integration: true
+  }
+);
 
-test('it renders with the correct class', function(assert) {
-  this.set('category', Ember.Object.create({
-    name: 'Food',
-    type: 'income'
-  }));
+test("it renders with the correct class", function(assert) {
+  this.set(
+    "category",
+    Ember.Object.create({
+      name: "Food",
+      type: "income"
+    })
+  );
   this.render(hbs`{{category-badge category=category}}`);
 
-  assert.equal(this.$('div.category--badge').length, 1, 'It has the \'.category--badge\' class');
+  assert.equal(
+    findAll("div.category--badge").length,
+    1,
+    "It has the '.category--badge' class"
+  );
 });
 
-test('it add a class based on the category type', function(assert) {
-  this.set('category', Ember.Object.create({
-    name: 'Food',
-    type: 'income'
-  }));
+test("it add a class based on the category type", function(assert) {
+  this.set(
+    "category",
+    Ember.Object.create({
+      name: "Food",
+      type: "income"
+    })
+  );
   this.render(hbs`{{category-badge category=category}}`);
 
-  assert.equal(this.$('div.category--badge-income').length, 1, 'It has the \'.category--badge-income\' class');
+  assert.equal(
+    findAll("div.category--badge-income").length,
+    1,
+    "It has the '.category--badge-income' class"
+  );
 
-  this.set('category', Ember.Object.create({
-    name: 'Food',
-    type: 'outcome'
-  }));
+  this.set(
+    "category",
+    Ember.Object.create({
+      name: "Food",
+      type: "outcome"
+    })
+  );
 
-  assert.equal(this.$('div.category--badge-outcome').length, 1, 'It has the \'.category--badge-outcome\' class');
+  assert.equal(
+    findAll("div.category--badge-outcome").length,
+    1,
+    "It has the '.category--badge-outcome' class"
+  );
 });
 
-test('it contains the abbrevation of the category name', function(assert) {
-  this.set('category', Ember.Object.create({
-    name: 'Food',
-    type: 'income'
-  }));
+test("it contains the abbrevation of the category name", function(assert) {
+  this.set(
+    "category",
+    Ember.Object.create({
+      name: "Food",
+      type: "income"
+    })
+  );
   this.render(hbs`{{category-badge category=category}}`);
 
-  assert.equal(this.$('div.category--badge').text().trim(), 'fd', '\'food\' is abbreviated in `fd`');
+  assert.equal(
+    find("div.category--badge").textContent.trim(),
+    "fd",
+    "'food' is abbreviated in `fd`"
+  );
 
-  this.set('category', Ember.Object.create({
-    name: 'Sport',
-    type: 'outcome'
-  }));
+  this.set(
+    "category",
+    Ember.Object.create({
+      name: "Sport",
+      type: "outcome"
+    })
+  );
 
-  assert.equal(this.$('span').text().trim(), 'sp', '\'sport\' is abbreviated in `sp`');
+  assert.equal(
+    find("div.category--badge").textContent.trim(),
+    "sp",
+    "'sport' is abbreviated in `sp`"
+  );
 });
