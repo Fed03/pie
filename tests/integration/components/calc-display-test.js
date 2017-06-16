3fa4594c35cd4264c79375bfa698931f5dab6d65
Federico Teotini <templarblack.fed03@gmail.com>
2017-06-16 12:17:39 +0200

    calc-display done

---
+++ tests/integration/components/calc-display-test.js
@@ -0,0 +1,52 @@
+import { findWithAssert } from "ember-native-dom-helpers";
+import { moduleForComponent, test } from "ember-qunit";
+import testSelector from "ember-test-selectors";
+import hbs from "htmlbars-inline-precompile";
+
+moduleForComponent("calc-display", "Integration | Component | calc display", {
+  integration: true
+});
+
+test("it renders", function(assert) {
+  this.render(hbs`{{calc-display}}`);
+
+  assert.ok(
+    findWithAssert(testSelector("calculator-display")).classList.contains(
+      "calculator-display"
+    ),
+    "It has the `calculator-display` class"
+  );
+});
+
+test("it displays the correct value", function(assert) {
+  this.render(hbs`{{calc-display value=value}}`);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "0",
+    "If value is undefined it starts at 0"
+  );
+
+  this.set("value", 123);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "123"
+  );
+
+  this.set("value", 123.5);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "123.5"
+  );
+
+  this.set("value", -123.5);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "-123.5"
+  );
+
+  this.set("value", -12345.6);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "-12,345.6"
+  );
+});
