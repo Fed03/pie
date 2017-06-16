2669f06fc4c5d19440e5c479c3e2d2dd41d80aba
Federico Teotini <templarblack.fed03@gmail.com>
2017-06-16 12:58:51 +0200

    number keys

--- tests/integration/components/calc-input-test.js
+++ tests/integration/components/calc-input-test.js
@@ -1,4 +1,4 @@
-import { findWithAssert } from "ember-native-dom-helpers";
+import { findWithAssert, click } from "ember-native-dom-helpers";
 import { moduleForComponent, test } from "ember-qunit";
 import testSelector from "ember-test-selectors";
 import hbs from "htmlbars-inline-precompile";
@@ -19,32 +19,52 @@ test("it renders", function(assert) {
 test("it displays the correct value", function(assert) {
   this.render(hbs`{{calc-input value=value}}`);
   assert.equal(
-    findWithAssert(testSelector("calculator-display")).textContent,
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
     "0",
     "If value is undefined it starts at 0"
   );
 
   this.set("value", 123);
   assert.equal(
-    findWithAssert(testSelector("calculator-display")).textContent,
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
     "123"
   );
 
   this.set("value", 123.5);
   assert.equal(
-    findWithAssert(testSelector("calculator-display")).textContent,
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
     "123.5"
   );
 
   this.set("value", -123.5);
   assert.equal(
-    findWithAssert(testSelector("calculator-display")).textContent,
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
     "-123.5"
   );
 
   this.set("value", -12345.6);
   assert.equal(
-    findWithAssert(testSelector("calculator-display")).textContent,
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
     "-12,345.6"
   );
+});
+
+test("it display value depending on click", async function(assert) {
+  this.render(hbs`{{calc-input value=value}}`);
+  assert.equal(this.get("value"), undefined);
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "0"
+  );
+
+  await click(testSelector("calc-key", 9));
+  await click(testSelector("calc-key", 8));
+  await click(testSelector("calc-key", 7));
+  await click(testSelector("calc-key", "dot"));
+  await click(testSelector("calc-key", 5));
+  assert.equal(
+    findWithAssert(testSelector("calculator-display")).textContent.trim(),
+    "987.5"
+  );
+  assert.equal(this.get("value"), undefined);
 });
