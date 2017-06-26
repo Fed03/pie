010e9684427300aaf2171be48c0dc91336bc9928
Federico Teotini <templarblack.fed03@gmail.com>
2017-06-26 23:13:22 +0200

    Hack to fix ember-pouch bug

--- app/routes/months/index.js
+++ app/routes/months/index.js
@@ -2,20 +2,32 @@ import Ember from "ember";
 import getDateForCurrentMonth from "pie/utils/get-date-for-current-month";
 
 export default Ember.Route.extend({
+  // beforeModel() {
+  //   const currentMonthDate = getDateForCurrentMonth();
+  //   return this.store
+  //     .queryRecord("month", {
+  //       filter: {
+  //         date: currentMonthDate
+  //       }
+  //     })
+  //     .then(async month => {
+  //       if (!month) {
+  //         month = await this._createCurrentMonth();
+  //       }
+  //       this.transitionTo("months.view", month);
+  //     });
+  // },
   beforeModel() {
     const currentMonthDate = getDateForCurrentMonth();
-    return this.store
-      .queryRecord("month", {
-        filter: {
-          date: currentMonthDate
-        }
-      })
-      .then(async month => {
-        if (!month) {
-          month = await this._createCurrentMonth();
-        }
-        this.transitionTo("months.view", month);
+    return this.store.findAll("month").then(async months => {
+      let currentMonth = months.find(month => {
+        return month.get("date").getTime() === currentMonthDate.getTime();
       });
+      if (!currentMonth) {
+        currentMonth = await this._createCurrentMonth();
+      }
+      this.transitionTo("months.view", currentMonth);
+    });
   },
 
   _createCurrentMonth() {
