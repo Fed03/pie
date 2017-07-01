680abc2dd797406106405816bedb8d053267d8f6
Federico Teotini <templarblack.fed03@gmail.com>
2017-07-01 21:40:33 +0200

    Finished datepicker component

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -1,11 +1,30 @@
 import Ember from "ember";
 
+const { isPresent, computed } = Ember;
+
 export default Ember.Component.extend({
   "data-test-date-picker": true,
   modalOpened: false,
+  displayValue: computed("internalValue", {
+    get() {
+      let value = this.get("internalValue");
+      if (isPresent(value)) {
+        return value.toLocaleDateString("en-US");
+      }
+    }
+  }),
+  internalValue: computed.oneWay("value"),
+  calendarCenter: computed.oneWay("value"),
   actions: {
     toggleModal() {
       this.toggleProperty("modalOpened");
+    },
+    dateSelected(date) {
+      this.set("internalValue", date);
+      if (this.get("onSelect")) {
+        this.get("onSelect")(date);
+      }
+      this.send("toggleModal");
     }
   }
 });
--- tests/integration/components/date-picker-test.js
+++ tests/integration/components/date-picker-test.js
@@ -1,10 +1,14 @@
 import { moduleForComponent, test } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
-import { click, findWithAssert, find } from "ember-native-dom-helpers";
+import { click, findWithAssert, find, findAll } from "ember-native-dom-helpers";
+import { calendarSelect, initCalendarHelpers } from "ember-power-calendar/test-support";
 import testSelector from "ember-test-selectors";
 
 moduleForComponent("date-picker", "Integration | Component | date picker", {
-  integration: true
+  integration: true,
+  beforeEach() {
+    initCalendarHelpers(this.container);
+  }
 });
 
 test("it renders", function(assert) {
@@ -29,11 +33,60 @@ test("the modal has an overlay", async function(assert) {
   assert.ok(find(".ember-modal-overlay"));
 });
 
-test("it closes the modal when clicking on the overlay", async function(
-  assert
-) {
+test("it closes the modal when clicking on the overlay", async function(assert) {
   this.render(hbs`{{date-picker}}`);
   await click(testSelector("date-picker-input"));
   await click(".ember-modal-overlay");
   assert.notOk(find(".ember-modal-dialog"));
+});
+
+test("it displays a calendar", async function(assert) {
+  assert.expect(0);
+  this.render(hbs`{{date-picker}}`);
+  await click(testSelector("date-picker-input"));
+  findWithAssert(".ember-power-calendar");
+});
+
+test("it sets the input val on date selection", async function(assert) {
+  this.render(hbs`{{date-picker}}`);
+  await click(testSelector("date-picker-input"));
+  await calendarSelect(".ember-power-calendar", new Date(2017, 6, 2));
+
+  assert.equal(find(testSelector("date-picker-input")).value, "7/2/2017");
+});
+
+test("it sends an action on date selection", async function(assert) {
+  assert.expect(1);
+  let date = new Date(2017, 6, 2);
+  this.on("select", selectedDate => {
+    assert.propEqual(selectedDate, date);
+  });
+
+  this.render(hbs`{{date-picker onSelect=(action "select")}}`);
+  await click(testSelector("date-picker-input"));
+  await calendarSelect(".ember-power-calendar", date);
+});
+
+test("it sets initial value", async function(assert) {
+  let date = new Date(2016, 6, 2);
+  this.set("date", date);
+  this.render(hbs`{{date-picker value=date}}`);
+
+  assert.equal(find(testSelector("date-picker-input")).value, "7/2/2016");
+  await click(testSelector("date-picker-input"));
+
+  assert.ok(findWithAssert('[data-date="2016-07-02"]').classList.contains("ember-power-calendar-day--selected"));
+});
+
+test("it closes the modal when a day is selected", async function(assert) {
+  this.render(hbs`{{date-picker}}`);
+  await click(testSelector("date-picker-input"));
+  await calendarSelect(".ember-power-calendar", new Date(2017, 6, 2));
+  assert.notOk(find(".ember-modal-dialog"));
+});
+
+test("the calendar can change month", async function(assert) {
+  this.render(hbs`{{date-picker}}`);
+  await click(testSelector("date-picker-input"));
+  assert.equal(findAll(".ember-power-calendar-nav-control").length, 2, "The calendar has arrows to change month");
 });
