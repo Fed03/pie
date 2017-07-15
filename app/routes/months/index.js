7a47c4697d76ef633a0138900902f18bd411124b
Federico Teotini <templarblack.fed03@gmail.com>
2017-07-15 17:52:26 +0200

    Using month-service in months.index route

--- app/routes/months/index.js
+++ app/routes/months/index.js
@@ -1,36 +1,10 @@
 import Ember from "ember";
-import getDateForCurrentMonth from "pie/utils/get-date-for-current-month";
 
 export default Ember.Route.extend({
-  // beforeModel() {
-  //   const currentMonthDate = getDateForCurrentMonth();
-  //   return this.store
-  //     .queryRecord("month", {
-  //       filter: {
-  //         date: currentMonthDate
-  //       }
-  //     })
-  //     .then(async month => {
-  //       if (!month) {
-  //         month = await this._createCurrentMonth();
-  //       }
-  //       this.transitionTo("months.view", month);
-  //     });
-  // },
-  beforeModel() {
-    const currentMonthDate = getDateForCurrentMonth();
-    return this.store.findAll("month").then(async months => {
-      let currentMonth = months.find(month => {
-        return month.get("date").getTime() === currentMonthDate.getTime();
-      });
-      if (!currentMonth) {
-        currentMonth = await this._createCurrentMonth();
-      }
-      this.transitionTo("months.view", currentMonth);
-    });
-  },
+  monthsService: Ember.inject.service(),
 
-  _createCurrentMonth() {
-    return this.store.createRecord("month").save();
+  async beforeModel() {
+    let currentMonth = await this.get("monthsService").findCurrentMonth();
+    this.transitionTo("months.view", currentMonth);
   }
 });
