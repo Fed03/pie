f807f9b15bd155db663f3304e56653c917fa2120
Federico Teotini <templarblack.fed03@gmail.com>
2017-09-01 21:32:27 +0200

    roboto-fontface

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,28 @@
 /* eslint-env node */
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     nodeAssets: {
-      'muicss': {
-        srcDir: 'dist',
-        import: ['css/mui.min.css', 'js/mui.min.js']
+      muicss: {
+        srcDir: "dist",
+        import: ["css/mui.min.css", "js/mui.min.js"]
+      },
+      "roboto-fontface": {
+        import: ["css/roboto/roboto-fontface.css"],
+        public: {
+          destDir: "/",
+          include: ["fonts/roboto/*"]
+        }
       }
     },
-    'ember-cli-babel': {
+    "ember-cli-babel": {
       includePolyfill: true
     }
   });
 
-  app.import('bower_components/Faker/build/build/faker.min.js', {
-    using: [
-      { transformation: 'amd', as: 'faker' }
-    ]
+  app.import("bower_components/Faker/build/build/faker.min.js", {
+    using: [{ transformation: "amd", as: "faker" }]
   });
 
   return app.toTree();
