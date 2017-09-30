167b2bf3e393afaa3408ebf233d8e7333c2efa6b
Federico Teotini <templarblack.fed03@gmail.com>
2017-09-30 19:32:21 +0200

    localDB creation

--- app/services/pouchdb-auth.js
+++ app/services/pouchdb-auth.js
@@ -1,4 +1,10 @@
-import Ember from 'ember';
+import Ember from "ember";
+import PouchDB from "pouchdb";
 
 export default Ember.Service.extend({
+  PouchDB,
+  init() {
+    this.options = this.options || {};
+    this.set("db", new this.PouchDB(this.options.localDb));
+  }
 });
--- tests/unit/services/pouchdb-auth-test.js
+++ tests/unit/services/pouchdb-auth-test.js
@@ -1,8 +1,17 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor } from "ember-qunit";
+import test from "ember-sinon-qunit/test-support/test";
 
-moduleFor('service:pouchdb-auth', 'Unit | Service | pouchdb auth', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+moduleFor("service:pouchdb-auth", "Unit | Service | pouchdb auth");
+
+test("it inits the db", function(assert) {
+  let dbInstance = {};
+  let spy = this.stub().returns(dbInstance);
+  let options = { localDb: "foo" };
+  let service = this.subject({ options, PouchDB: spy });
+
+  assert.ok(spy.calledWithNew());
+  assert.ok(spy.calledWithExactly("foo"));
+  assert.equal(service.get("db"), dbInstance);
 });
 
 // Replace this with your real tests.
