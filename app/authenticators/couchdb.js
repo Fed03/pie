7cd16a0381f0d44e793fcdb1e21edd830a86adb2
Federico Teotini <templarblack.fed03@gmail.com>
2017-10-05 19:34:22 +0200

    draft: couchdb authenticator

---
+++ app/authenticators/couchdb.js
@@ -0,0 +1,16 @@
+import Base from "ember-simple-auth/authenticators/base";
+import { inject } from "@ember/service";
+
+export default Base.extend({
+  pouchDbService: inject("pouchdb-auth"),
+  restore(data) {},
+
+  authenticate(username, password) {
+    const service = this.get("pouchDbService");
+    return service.login(username, password).then(() => {
+      return service.getUser();
+    });
+  },
+
+  invalidate(data) {}
+});
