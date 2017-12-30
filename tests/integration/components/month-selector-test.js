7c2575753cf2be9410e1da9db96d4f3a52275c07
Federico Teotini <templarblack.fed03@gmail.com>
2017-12-30 10:53:56 +0100

    Beginning of month-selector component

---
+++ tests/integration/components/month-selector-test.js
@@ -0,0 +1,17 @@
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
+import { find, findWithAssert } from "ember-native-dom-helpers";
+
+moduleForComponent("month-selector", "Integration | Component | month selector", {
+  integration: true
+});
+
+test("it renders the required html", function(assert) {
+  assert.expect(0);
+  this.render(hbs`{{month-selector}}`);
+
+  findWithAssert("[data-test-month-selector]");
+  findWithAssert("button[data-test-next-month-btn]");
+  findWithAssert("button[data-test-previous-month-btn]");
+  findWithAssert("[data-test-current-month]");
+});
