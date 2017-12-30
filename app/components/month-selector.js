22e897b248764c85641f6b967d695da407313e55
Federico Teotini <templarblack.fed03@gmail.com>
2017-12-30 12:19:00 +0100

    Displaying the current month name and year

--- app/components/month-selector.js
+++ app/components/month-selector.js
@@ -1,5 +1,9 @@
 import Component from "@ember/component";
+import momentComputed from "ember-moment/computeds/moment";
+import format from "ember-moment/computeds/format";
 
 export default Component.extend({
-  "data-test-month-selector": true
+  "data-test-month-selector": true,
+  currentMonthDate: momentComputed("currentMonth.date"),
+  currentMonthName: format("currentMonthDate", "MMMM YYYY"),
 });
--- tests/integration/components/month-selector-test.js
+++ tests/integration/components/month-selector-test.js
@@ -1,17 +1,30 @@
 import { moduleForComponent, test } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
 import { find, findWithAssert } from "ember-native-dom-helpers";
+import { manualSetup, make } from "ember-data-factory-guy";
 
 moduleForComponent("month-selector", "Integration | Component | month selector", {
-  integration: true
+  integration: true,
+  beforeEach() {
+    manualSetup(this.container);
+  }
 });
 
 test("it renders the required html", function(assert) {
   assert.expect(0);
-  this.render(hbs`{{month-selector}}`);
+  this.set("month", make("currentMonth"));
+  this.render(hbs`{{month-selector currentMonth=month}}`);
 
   findWithAssert("[data-test-month-selector]");
   findWithAssert("a[data-test-next-month-btn]");
   findWithAssert("a[data-test-previous-month-btn]");
   findWithAssert("[data-test-current-month]");
+});
+
+test("it displays the current month name and year", function(assert) {
+  let november2016 = new Date(2016, 10, 1, 0, 0, 0, 0);
+  this.set("month", make("month", { date: november2016 }));
+  this.render(hbs`{{month-selector currentMonth=month}}`);
+
+  assert.equal(find("[data-test-current-month]").textContent.trim(), "November 2016");
 });
