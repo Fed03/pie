d56fe929877e8f3b2d61d5a337b184bf0ee095c5
Federico Teotini <templarblack.fed03@gmail.com>
2017-12-30 12:19:31 +0100

    Fires actions when the link are clicked

--- app/components/month-selector.js
+++ app/components/month-selector.js
@@ -1,9 +1,42 @@
 import Component from "@ember/component";
 import momentComputed from "ember-moment/computeds/moment";
 import format from "ember-moment/computeds/format";
+import moment from "moment";
 
 export default Component.extend({
   "data-test-month-selector": true,
   currentMonthDate: momentComputed("currentMonth.date"),
   currentMonthName: format("currentMonthDate", "MMMM YYYY"),
+
+  actions: {
+    prevMonth() {
+      this.sendAction("prevMonthAction", this._prevDate(), this.get("currentMonth"));
+    },
+    nextMonth() {
+      this.sendAction("nextMonthAction", this._nextDate(), this.get("currentMonth"));
+    }
+  },
+
+  _prevDate() {
+    return this._getPrevOrNextMonth("prev");
+  },
+
+  _nextDate() {
+    return this._getPrevOrNextMonth("next");
+  },
+
+  _getPrevOrNextMonth(direction) {
+    let month = this.get("currentMonthDate").clone();
+    let duration = moment.duration(1, "months");
+    switch (direction) {
+      case "prev":
+        month.subtract(duration);
+        break;
+      case "next":
+        month.add(duration);
+        break;
+    }
+
+    return month.toDate();
+  }
 });
--- tests/integration/components/month-selector-test.js
+++ tests/integration/components/month-selector-test.js
@@ -1,6 +1,7 @@
 import { moduleForComponent, test } from "ember-qunit";
 import hbs from "htmlbars-inline-precompile";
-import { find, findWithAssert } from "ember-native-dom-helpers";
+import moment from "moment";
+import { find, findWithAssert, click } from "ember-native-dom-helpers";
 import { manualSetup, make } from "ember-data-factory-guy";
 
 moduleForComponent("month-selector", "Integration | Component | month selector", {
@@ -27,4 +28,32 @@ test("it displays the current month name and year", function(assert) {
   this.render(hbs`{{month-selector currentMonth=month}}`);
 
   assert.equal(find("[data-test-current-month]").textContent.trim(), "November 2016");
+});
+
+test("it fires action when changing month", async function(assert) {
+  assert.expect(4);
+  let november2016 = new Date(2016, 10, 1, 0, 0, 0, 0);
+  let currentMonth = make("month", { date: november2016 });
+  this.set("currentMonth", currentMonth);
+
+  this.on("prev", (date, month) => {
+    let expectedDate = moment(november2016)
+      .subtract(1, "months")
+      .toDate();
+    assert.deepEqual(month, currentMonth, "The passed month is equal to the currentMonth");
+    assert.equal(date.getTime(), expectedDate.getTime());
+  });
+
+  this.on("next", (date, month) => {
+    let expectedDate = moment(november2016)
+      .add(1, "months")
+      .toDate();
+    assert.deepEqual(month, currentMonth, "The passed month is equal to the currentMonth");
+    assert.equal(date.getTime(), expectedDate.getTime());
+  });
+
+  this.render(hbs`{{month-selector currentMonth=currentMonth nextMonthAction=(action 'next') prevMonthAction=(action 'prev')}}`);
+
+  await click("[data-test-previous-month-btn]");
+  await click("[data-test-next-month-btn]");
 });
