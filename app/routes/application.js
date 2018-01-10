862326c5097d4f1b72c4f8626e1d02ee64f375d3
Federico Teotini <templarblack.fed03@gmail.com>
2018-01-10 12:47:12 +0100

    Cat seeds only when auth

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,39 +2,46 @@ import { all } from "rsvp";
 import Route from "@ember/routing/route";
 import { inject } from "@ember/service";
 import faker from "faker";
+import { run } from "@ember/runloop";
 import ApplicationRouteMixin from "ember-simple-auth/mixins/application-route-mixin";
 
 export default Route.extend(ApplicationRouteMixin, {
   currentUser: inject(),
+  session: inject(),
   routeAfterAuthentication: "months.index",
   beforeModel() {
     return all([this._createDefaultCategories(), this.get("currentUser").load()]);
   },
   sessionAuthenticated() {
     let superMethod = this._super.bind(this, ...arguments);
+    this._createDefaultCategories();
     this.get("currentUser")
       .load()
       .then(() => {
         superMethod();
       });
   },
-  _createDefaultCategories() {
+  async _createDefaultCategories() {
     //TODO adjust this
-    return this.store.findAll("category").then(categories => {
-      if (categories.get("length") === 0) {
+    let promise = Promise.resolve();
+    if (this.get("session.isAuthenticated")) {
+      let catNumber = (await this.store.findAll("category")).get("length");
+      if (catNumber === 0) {
         let promises = [];
         let type = ["income", "outcome"];
         for (var i = 0; i < 10; i++) {
-          let cat = this.store.createRecord("category", {
-            name: faker.commerce.department(),
-            type: type[i % 2]
+          run(() => {
+            let cat = this.store.createRecord("category", {
+              name: faker.commerce.department(),
+              type: type[i % 2]
+            });
+            promises.push(cat.save());
           });
-
-          promises.push(cat.save());
         }
-        return all(promises);
+        promise = all(promises);
       }
-      return Promise.resolve();
-    });
+    }
+
+    return promise;
   }
 });
--- tests/acceptance/categories-seed-test.js
+++ tests/acceptance/categories-seed-test.js
@@ -1,6 +1,7 @@
 import { test } from "qunit";
 import { visit } from "ember-native-dom-helpers";
 import moduleForPouchAcceptance from "pie/tests/helpers/module-for-pouch-acceptance";
+import { authAndLoadUser } from "pie/tests/helpers/auth-and-load-user";
 
 moduleForPouchAcceptance("Acceptance | categories seed", {
   beforeEach() {
@@ -8,11 +9,29 @@ moduleForPouchAcceptance("Acceptance | categories seed", {
   }
 });
 
-test("it seeds default categories", async function(assert) {
+test("when already authenticated, it seeds the categories", async function(assert) {
+  await authAndLoadUser(this.application);
+  // let categories = await this.store.findAll("category");
+  // assert.equal(categories.get("length"), 0, "There are no categories in store");
+
+  await visit("/");
+  let categories = await this.store.findAll("category");
+  assert.ok(categories.get("length") > 0, "There are few categories seeded");
+});
+
+test("when not authenticated, it does not seed the categories", async function(assert) {
+  await visit("/");
+  let categories = await this.store.findAll("category");
+  assert.equal(categories.get("length"), 0, "There are few categories seeded");
+});
+
+test("when gets authenticated, it seeds the categories", async function(assert) {
+  await visit("/");
   let categories = await this.store.findAll("category");
   assert.equal(categories.get("length"), 0, "There are no categories in store");
 
-  await visit("/");
+  await authAndLoadUser(this.application);
+
   categories = await this.store.findAll("category");
   assert.ok(categories.get("length") > 0, "There are few categories seeded");
 });
